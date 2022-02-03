863c6fb4e70121d819bde7022157e359c97c58a8
MV <matiasvogel91@gmail.com>
2022-02-03 13:17:44 -0300

    Add project files.

---
+++ CoinFlip/wwwroot/script.js
@@ -0,0 +1,60 @@
+﻿const tags = document.getElementsByClassName('tags');
+const textarea = document.getElementById('textarea');
+const button = document.getElementById('button');
+const title = document.getElementById('title');
+const coin = document.getElementById('coin');
+
+let state = 0;
+
+textarea.focus();
+
+textarea.addEventListener('keyup', (e) => {
+    createTag(e.target.value)
+});
+
+function createTag(input) {
+    tags[state].innerText = input;
+        
+}
+
+button.addEventListener("click", () => {
+    state++;
+
+    switch (state) {
+        case 1:
+            textarea.value = "";
+            textarea.focus();
+            title.innerText = "Enter 2nd Choice";
+            break;
+        case 2:
+            button.textContent = "FLIP!";
+            title.innerText = "";
+            textarea.value = "";
+            textarea.disabled = true;
+            textarea.placeholder = "";
+            break;
+        case 3:
+            coin.classList.remove('hidden');
+            setTimeout(choose, 1100)
+            button.disabled = true;
+            break;
+        default:
+            location.reload(true);
+    }
+})
+
+function choose() {
+    coin.classList.add('hidden');
+    let chosen = Math.floor(Math.random() * 2);
+    tags[chosen].classList.remove('badge-warning');
+    tags[chosen].classList.add('badge-success');
+    let notChosen = chosen == 0 ? 1 : 0;
+    tags[notChosen].classList.remove('badge-warning');
+    tags[notChosen].classList.add('badge-danger');
+    button.innerText = "Try again"
+    button.disabled = false;
+}
+
+
+
+
